2ce63cea006ca852480b1ec6fcf906c421526753
Dohee Lee <dojuhee2015@gmail.com>
2023-10-28 23:00:24 +0100

    change the colour of the toggle icon into brown

--- src/component/Navbar/Navbar.styled.jsx
+++ src/component/Navbar/Navbar.styled.jsx
@@ -5,19 +5,21 @@ export const NavbarWrapper = styled.div`
   background-color: ${theme.color.lightBrown};
 
   .logo {
-    width: 70px;
-    height: 70px;
+    width: 4rem;
+    height: 4rem;
     margin-top: 8px;
   }
 
   .homepage-toggle {
-    color: ${theme.color.brown} !important;
     border: none !important;
-    font-size: 30px;
+    font-size: 1.8rem;
+  }
+  .navbar-toggler-icon {
+    background-image: url('data:image/svg+xml,%3csvg xmlns=%27http://www.w3.org/2000/svg%27 viewBox=%270 0 30 30%27%3e%3cpath stroke=%27%23623D2E%27 stroke-linecap=%27round%27 stroke-miterlimit=%2710%27 stroke-width=%272%27 d=%27M4 7h22M4 15h22M4 23h22%27/%3e%3c/svg%3e');
   }
 
   .homepage-toggle:focus {
-    box-shadow: 0 0 0 2px ${theme.color.lightBrown};
+    box-shadow: 0 0 0 2px ${theme.color.lightBrown} !important;
   }
 
   .homepage-nav {
@@ -45,7 +47,7 @@ export const NavbarWrapper = styled.div`
     color: ${theme.color.white};
     font-weight: 900;
     text-decoration: none;
-    font-size: 20px;
+    font-size: 1.4rem;
   }
 
   a:hover {
