15fb7ab608ae2f6f2a068247210cb92c6deacff7
Dohee Lee <dojuhee2015@gmail.com>
2024-01-10 23:10:16 +0000

    pass all the tests

--- src/component/Text/Text.test.jsx
+++ src/component/Text/Text.test.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { MemoryRouter } from 'react-router-dom'
-import { render, screen } from '@testing-library/react'
+import { fireEvent, render, screen, waitFor } from '@testing-library/react'
 import Text from './Text'
 
 describe('Text component', () => {
@@ -13,5 +13,36 @@ describe('Text component', () => {
     const textHeading = screen.getByRole('heading')
     expect(textHeading).toBeInTheDocument()
     expect(textHeading).toHaveTextContent(/welcome to my portfolio/i)
+  })
+
+  it('should have the buttons called with explore projects and my resume', () => {
+    render(
+      <MemoryRouter>
+        <Text />
+      </MemoryRouter>
+    )
+    const exploreProjectButton = screen.getByRole('button', {
+      name: /explore projects/i,
+    })
+    const myResumeButton = screen.getByRole('button', { name: /my resume/i })
+
+    expect(exploreProjectButton).toBeInTheDocument()
+    expect(myResumeButton).toBeInTheDocument()
+  })
+
+  it('should navigate to the project page', async () => {
+    render(
+      <MemoryRouter>
+        <Text />
+      </MemoryRouter>
+    )
+    const exploreProjectButton = screen.getByRole('button', {
+      name: /explore projects/i,
+    })
+
+    fireEvent.click(exploreProjectButton)
+    await waitFor(() => {
+      expect(screen.getByText('Welcome To My Portfolio')).toBeInTheDocument()
+    })
   })
 })
